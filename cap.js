5654c27c2ba01b3dd35b5be25b203f81911d2eaa
DatSwede <88003385+DatSwede@users.noreply.github.com>
2023-10-12 15:54:15 -0700

    Create cap.js

---
+++ cap.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById('form-1');
+  
+  if (form) {
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      
+      const now = new Date().getTime();
+      const twelveHours = 12 * 60 * 60 * 1000;  // 12 hours in milliseconds
+      let submissions = JSON.parse(localStorage.getItem('form-1-submissions') || "[]");
+      
+      // Filter out timestamps older than 12 hours
+      submissions = submissions.filter(timestamp => now - timestamp < twelveHours);
+      
+      if (submissions.length >= 5) {
+        alert('You can only submit 5 times every 12 hours.');
+        return false;
+      } else {
+        // Add current timestamp to the submissions array
+        submissions.push(now);
+        localStorage.setItem('form-1-submissions', JSON.stringify(submissions));
+        form.submit(); // Submit the form
+      }
+    });
+  }
+});
